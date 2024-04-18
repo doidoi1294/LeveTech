c7e48a3929629c9e70cce4518fbd2425645090ff
doidoi1294 <h520524b@mails.cc.ehime-u.ac.jp>
2024-04-18 13:26:49 +0900

    dev_basis02 pull

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
+
+// 直接Viewファイルを指定
+// Route::get('/', function() {
+//     return view('posts.index');
+// });
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
