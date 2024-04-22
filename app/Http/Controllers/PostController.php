36f1b949d38b0a32c08fc6dfd3c38b2f13bbb0c5
doidoi1294 <h520524b@mails.cc.ehime-u.ac.jp>
2024-04-22 12:00:13 +0900

    dev_basis05

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,7 +28,7 @@ class PostController extends Controller
         return view('posts.show')->with(['post' => $post]);
         //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
     }
-    
+    // 投稿作成画面
     public function store(Post $post, PostRequest $request){
         // リクエストパラメータの取得
         $input = $request['post'];
@@ -36,6 +36,17 @@ class PostController extends Controller
         $post->fill($input)->save();
         // リダイレクト
         return redirect('/posts/'.$post->id);
+    }
+    // 編集画面(idの値の受け渡し)
+    public function edit(Post $post){
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    // 編集画面(投稿作成画面と同様の処理)
+    public function update(PostRequest $request, Post $post){
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+    
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
